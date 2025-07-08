7f0da9c52ec596a7138c643450514cb31ffd144e
yavasonur1 <onur.yavas@inventanalytics.com>
2025-07-08 10:01:48 +0300

    send parameter to lambda func

--- maps-runner/app.py
+++ maps-runner/app.py
@@ -1,5 +1,6 @@
 def lambda_handler(event, context):
+    name = event.get('name', 'World')
     return {
         'statusCode': 200,
-        'body': 'Hello World!'
+        'body': f'Hello {name}!'
     }
