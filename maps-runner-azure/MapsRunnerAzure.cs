f82bdff7ac80a7520e324064976db101c26602d3
Onur Yavas <onur.yavas@inventanalytics.com>
2025-08-06 11:42:32 +0300

    change azure function for trigger aci

--- maps-runner-azure/MapsRunnerAzure.cs
+++ maps-runner-azure/MapsRunnerAzure.cs
@@ -5,6 +5,10 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.ContainerInstance;
+using Azure.ResourceManager.ContainerInstance.Models;
 
 namespace AzureFunctionApp
 {
@@ -24,16 +28,23 @@ namespace AzureFunctionApp
 
             try
             {
-                string keyVaultUrl = "https://your-keyvault-name.vault.azure.net/";
-
+                // Key Vault erişimi
+                /*string keyVaultUrl = "https://your-keyvault-name.vault.azure.net/";
                 var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
 
                 string dbServer = (await client.GetSecretAsync("DbServer")).Value.Value;
                 string dbName = (await client.GetSecretAsync("DbName")).Value.Value;
                 string dbUser = (await client.GetSecretAsync("DbUser")).Value.Value;
-                string dbPassword = (await client.GetSecretAsync("DbPassword")).Value.Value;
+                string dbPassword = (await client.GetSecretAsync("DbPassword")).Value.Value;*/
+
+                string dbServer = "74.234.169.223,1433";
+                string dbName = "PreviewEnvironmentDB";
+                string dbUser = "previewenvuser";
+                string dbPassword = "Onur123456789";
 
                 string connectionString = $"Server=tcp:{dbServer},1433;Initial Catalog={dbName};Persist Security Info=False;User ID={dbUser};Password={dbPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                int runId;
 
                 using (var conn = new SqlConnection(connectionString))
                 {
@@ -48,13 +59,68 @@ namespace AzureFunctionApp
                         return;
                     }
 
-                    int runId = (int)runIdObj;
-                    _logger.LogInformation($"Processed RunId: {runId}");
+                    runId = (int)runIdObj;
+                    _logger.LogInformation($"Processing RunId: {runId}");
 
                     var updateCmd = new SqlCommand("UPDATE engine.AlgoRuns SET Status = 6 WHERE RunId = @runId", conn);
                     updateCmd.Parameters.AddWithValue("@runId", runId);
                     await updateCmd.ExecuteNonQueryAsync();
                 }
+
+                // ACI başlat
+                string subscriptionId = Environment.GetEnvironmentVariable("90b891f0-79da-4298-9f6a-a955ef06c8f8");
+                string resourceGroupName = "acme-dev-rg";
+                string containerGroupName = $"run-{runId}-{DateTime.UtcNow:yyyyMMddHHmmss}";
+                string containerName = "preview-env-container-instance";
+                string location = "westeurope";
+                string acrServer = "inventpreviewenv.azurecr.io";
+                string imageName = "mapsunified-preview-repository";
+                string acrImage = $"{acrServer}/{imageName}:latest";
+
+                var credential = new DefaultAzureCredential();
+                var armClient = new ArmClient(credential, subscriptionId);
+
+                // Resource Group referansı al
+                var resourceGroup = armClient.GetResourceGroupResource(
+                    ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName));
+
+                // ACI tanımı
+                var containerGroupData = new ContainerGroupData(location)
+                {
+                    OsType = ContainerGroupOsType.Linux,
+                    RestartPolicy = ContainerGroupRestartPolicy.Never,
+                    Containers =
+                    {
+                        new ContainerInstanceContainer(containerName, acrImage, new ContainerResourceRequirements
+                        {
+                            Requests = new ContainerResourceRequestsContent(1.0, 1.5)
+                        })
+                        {
+                            Command =
+                            {
+                                "dotnet", "Maps.Runner.dll",
+                                "--run-id", runId.ToString(),
+                                "--customer-name", "integrationtest",
+                                "--environment-type", "preview"
+                            }
+                        }
+                    },
+                    // ImageRegistryCredentials kısmı yok çünkü MSI ile erişim sağlıyoruz
+                    Identity = new ContainerGroupManagedServiceIdentity(ManagedServiceIdentityType.UserAssigned)
+                    {
+                        UserAssignedIdentities =
+                        {
+                            { new ResourceIdentifier("/subscriptions/90b891f0-79da-4298-9f6a-a955ef06c8f8/resourceGroups/acme-dev-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/preview-env-managed-identity"), new UserAssignedIdentity() }
+                        }
+                    }
+                };
+
+                // ACI oluştur
+                await resourceGroup.GetContainerGroups()
+                    .CreateOrUpdateAsync(WaitUntil.Completed, containerGroupName, containerGroupData);
+
+                _logger.LogInformation($"ACI created: {containerGroupName} for runId {runId}");
+
             }
             catch (Exception ex)
             {
